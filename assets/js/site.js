9a5186768abd34d02ece2869ec4f0044079159f0
David Hatcher <sallazar024@gmail.com>
2019-04-27 11:53:55 -0400

    Uploading Offline Tests
    uploading offline tests for availability

--- assets/js/site.js
+++ assets/js/site.js
@@ -30,8 +30,16 @@ window.onload = function () {
 			}, i * 70);
 		}
 
+		// for (var i = 0; i < logoTitle.length + 1; i++) {
+		// 	logoRandom = logoTitle.substr(0, i);
+		// 	for (var j = i; j < logoTitle.length; j++) {
+		// 		logoRandom += possible.charAt(Math.floor(Math.random() * possible.length));
+		// 	}
+		// 	generateRandomTitle(i, logoRandom);
+		// 	logoRandom = '';
+		// }
 		for (var i = 0; i < logoTitle.length + 1; i++) {
-			logoRandom = logoTitle.substr(0, i);
+			logoRandom = logoTitle.substr(i, 0);
 			for (var j = i; j < logoTitle.length; j++) {
 				logoRandom += possible.charAt(Math.floor(Math.random() * possible.length));
 			}
